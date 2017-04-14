1fc1034729badfa19bed6bdb572f9f3f5ddc093d
MOON <princeps90@gmail.com>
2017-04-14 23:53:29 +0900

    all solved

--- src/prob5/Prob5.java
+++ src/prob5/Prob5.java
@@ -4,32 +4,45 @@ import java.util.*;
 public class Prob5 {
 
 	public static void main(String[] args) {
-		int count=0;
+		
 		Random r = new Random();
 		
-		//int rnum = r.nextInt(100)+1;
-		int rnum=5;
+		int rnum = r.nextInt(100)+1;
+		//int rnum=5;
 		Scanner sc = new Scanner(System.in);
 		boolean flag=true;
+		int count=1;
+		int min = 1;
+		int max = 100;
+		
 		while(flag){
 		System.out.print("숫자입력 :");
 		int input = sc.nextInt();
 		
+
 		if(rnum==input){
 			System.out.println("맞았습니다. 다시하실건가요?");
 			String answer=sc.next();
 			if(answer.equals("y") ){
+				count=0;
+				min = 1;
+				max = 100;
+				return ;
+				
+			}else if(answer.equals("n")){
 				flag=false;
 			}
 		}else if(rnum < input){
-			System.out.println(input+"-"+100);
+			System.out.println(min+ "-" +max);
 			System.out.println(count+">>"+input);
-			System.out.println("틀렸습니다. 더낮게");
+			System.out.println("더낮게");
+			max = input;
 			count++;
 		}else if(rnum > input){
-			System.out.println(input+"-"+100);
+			System.out.println(min+"-"+max);
 			System.out.println(count+">>"+input);
-			System.out.println("틀렸습니다. 더높게");
+			System.out.println("더높게");
+			min = input;
 			count++;
 		}
 	
